c9d750108175a87ae3868390a5841db1979c3fba
reham ayman <rehamayman15@icloud.com>
2024-09-24 05:21:58 +0300

    socket init

---
+++ Takhawi/Application/make a trip /findingAdriver/findingDriver+Socket.swift
@@ -0,0 +1,8 @@
+//
+//  findingDriver+Socket.swift
+//  Takhawi
+//
+//  Created by Reham Ayman on 19/03/1446 AH.
+//
+
+import Foundation
---
+++ Takhawi/Application/socketManager/socketManager.swift
@@ -0,0 +1,8 @@
+//
+//  socketManager.swift
+//  Takhawi
+//
+//  Created by Reham Ayman on 19/03/1446 AH.
+//
+
+import Foundation
