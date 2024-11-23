0b8ec91549f5697f22a58d462058acf28da0214d
reham ayman <rehamayman15@icloud.com>
2024-11-24 01:22:26 +0200

    push user notifications and verify id action

---
+++ Takhawi/Application/USER/sideMenu/profile/verifyId/verifiyIdSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  verifiyIdSwiftUIView.swift
+//  Takhawi
+//
+//  Created by Reham Ayman on 20/05/1446 AH.
+//
+
+import SwiftUI
+
+struct verifiyIdSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    verifiyIdSwiftUIView()
+}
